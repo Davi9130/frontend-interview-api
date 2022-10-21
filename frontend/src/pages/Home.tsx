15822e8c59a7b29ccdd41c6a7f431d0027cafd44
katryo <katoryo55@gmail.com>
2022-10-21 11:01:45 +0900

    Change the links

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -4,19 +4,28 @@ export const Home = () => {
       <h1>Home</h1>
       <ul>
         <li>
-          <a href="/v1/customer">V1 Customer</a>
+          <a href="/v1/customer?id=1">V1 Customer id:1</a>
         </li>
         <li>
-          <a href="/v2/customer">V2 Customer</a>
+          <a href="/v2/customer?id=1">V2 Customer id:1</a>
         </li>
         <li>
-          <a href="/v3/customer">V3 Customer</a>
+          <a href="/v2/customer?id=2">V2 Customer id:2</a>
         </li>
         <li>
-          <a href="/v4/customer">V4 Customer</a>
+          <a href="/v3/customer?id=1">V3 Customer id:1</a>
         </li>
         <li>
-          <a href="/v5/customer">V5 Customer</a>
+          <a href="/v3/customer?id=1">V3 Customer id:1</a>
+        </li>
+        <li>
+          <a href="/v3/customer?id=2">V3 Customer id:2</a>
+        </li>
+        <li>
+          <a href="/v4/customer?id=1">V4 Customer id:1</a>
+        </li>
+        <li>
+          <a href="/v5/customer?id=1">V5 Customer id:1</a>
         </li>
       </ul>
     </div>
