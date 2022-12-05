009fd4e3d47116652af52d5482e0e3cb37242ab6
MateusTorquato <mateus@sardine.ai>
2022-12-05 19:15:59 -0300

    Fixing V4 instructions

--- frontend/src/pages/CustomerV4.tsx
+++ frontend/src/pages/CustomerV4.tsx
@@ -1,5 +1,8 @@
 // Question 4. Please copy and paste your CustomerV3 code.
-// Please make the web page beautiful. You can use any libraries you like.
+// Please define a new custom hook useFetchCustomerResult in a new file and use it in CustomerV4.tsx.
+// useFetchCustomerResult fetches the data from the endpoint and handles errors.
+// In this way, you will no longer need to call useEffect in CustomerV4.tsx.
+
 export const CustomerV4 = () => {
   return <h1>CustomerV4</h1>;
 };
